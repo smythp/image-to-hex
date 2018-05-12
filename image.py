d95ced79a4a456d8c4882fa0d72d7758505fb577
Patrick Smyth <patricksmyth01@gmail.com>
2018-05-12 16:13:34 -0400

    Initial commit

---
+++ image.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+im = Image.open('test.png')
+
+pixels = im.load()
+
+x_pixels, y_pixels = im.size
+
+output_array = []
+
+for y_pixel in range(0, y_pixels):
+    x_array = []
+
+    for x_pixel in range(0, x_pixels):
+        rgb_value = pixels[x_pixel, y_pixel]
+
+        hex_code = '#%02x%02x%02x' % rgb_value[:3]
+
+        x_array.append(hex_code)
+
+    output_array.append(x_array)
+
+                    
+with open('hex.txt', 'w') as out_file:
+    for x_row in output_array:
+        for hex in x_row:
+            out_file.write(hex + ' ')
